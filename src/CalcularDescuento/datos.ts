5c7aba7d7266656cd2e5e09ba8368cbb1a21d1fd
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-05-05 17:00:23 -0300

    Descuentodel10 cambio de la constante del porcentaje de descuento

--- src/CalcularDescuento/datos.ts
+++ src/CalcularDescuento/datos.ts
@@ -1,7 +1,7 @@
 let precioProducto: number = 450.5;
-let porcentajeDescuento: number = 0.1;
-let descuento: number = precioProducto * porcentajeDescuento;
-let precioFinal: number = precioProducto - descuento;
+let porcentajeDescuento: number = 0.9;
+/*let descuento: number = precioProducto * porcentajeDescuento;*/
+let precioFinal: number = precioProducto * porcentajeDescuento;
 btnPrecioFinal.addEventListener("click", () => {
   console.log("El Descuento es:", precioFinal);
 });
