662549ed85509b73a231fc8ae8aa8939f39aa616
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-05-12 23:30:28 -0300

    Descuento 10% en general Ahora se puede aplicar un descuento del 10 % a cualquier valor

--- src/CalcularDescuento/datos.ts
+++ src/CalcularDescuento/datos.ts
@@ -1,7 +1,15 @@
-let precioProducto: number = 450.5;
+let precioProducto = document.getElementById("precioProducto");
+let btnPrecioFinal = document.getElementById("btnPrecioFinal");
+
+btnPrecioFinal.addEventListener("click", () => {
+  let ingresarPrecio: number = Number(precioProducto.value);
+  console.log("El Descuento es:", ingresarPrecio * 0.9);
+});
+
+/*let precioProducto: number = 450.5;
 let porcentajeDescuento: number = 0.9;
 /*let descuento: number = precioProducto * porcentajeDescuento;*/
-let precioFinal: number = precioProducto * porcentajeDescuento;
+/*let precioFinal: number = precioProducto * porcentajeDescuento;
 btnPrecioFinal.addEventListener("click", () => {
   console.log("El Descuento es:", precioFinal);
-});
+});*/
